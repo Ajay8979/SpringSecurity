1202c834a669be57894f3ac360f0af37023fa453
Ajay Singh <sajay@corp.ojas-it.com>
2020-01-28 19:33:30 +0530

    first commit

---
+++ src/main/java/net/dontdrinkandroot/example/angularrestspringsecurity/JsonViews.java
@@ -0,0 +1,15 @@
+package net.dontdrinkandroot.example.angularrestspringsecurity;
+
+/**
+ * @author Philip Washington Sorst <philip@sorst.net>
+ */
+public class JsonViews
+{
+    public static class User
+    {
+    }
+
+    public static class Admin extends User
+    {
+    }
+}
